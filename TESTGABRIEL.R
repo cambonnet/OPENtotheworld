2e0805baedbdf23a191a29efaf5049b54595bd31
GABRIEL <gburellier@etu.isara.fr>
2024-02-13 16:55:29 +0100

    ajout du fichier test if et boucle de gab

---
+++ TESTGABRIEL.R
@@ -0,0 +1,62 @@
+###fin script app pour les résultats desti
+
+tabPanel(class="intro", title = "", textOutput("message3")),
+tabPanel(class="intro", title = "", textOutput("message4"))
+##
+output$message3 <- renderDataTable({    
+  req(validate_click())
+  if (input$enfant < 1 ){
+    return(test1 <-filter(pay ,Enfants== "Oui"))
+  } else {
+    return(test1 <-filter(pay ,Enfants== "Non"))
+  }
+}
+)
+output$message4 <- renderText({    
+  req(validate_click())
+  paste(test1[1:5,1],test1[1:5,1])})
+
+#############################################
+###Test filter :
+
+library(readxl)
+library(dplyr)
+pay <- read_excel("pays.xlsx")
+dim(pay)
+head(pay)
+
+pay[3,7]
+
+if (enfant<=1)
+  test1<- filter(pay, Culturelles == "Oui")
+test1
+test2 <-filter(test1,Enfants== "Oui")
+test2
+test3 <-filter(test2,Détentes== "Oui")
+test3 
+test4 <-filter(test3,Festives == "Oui")
+test4
+
+Result <- test3[1:5,1]
+Result
+
+###tests if
+
+(if (test1 == "Oui"){
+  return("test oui c'est culturelle")}
+  else 
+  {return("test non c'est pas cultu")})
+
+pay[,7]
+test1<- filter(pay[,7])
+if (test1 == "Oui"){
+  return("test oui c'est culturelle")}
+else {
+  return("test non c'est pas cultu")}
+
+x <- 1:100
+filter(x, rep(1, 3))
+filter(x, rep(1, 3), sides = 1)
+filter(x, rep(1, 3), sides = 1, circular = TRUE)
+
+filter(presidents, rep(1, 3))
