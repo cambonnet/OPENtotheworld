b85a2468205420f24ac7e709218dd3986e4d4aa1
cambonnet <cbonnetu@etu.isara.com>
2024-02-15 08:37:46 +0100

    version de base camille

--- essaie_1.R
+++ essaie_1.R
@@ -1,9 +1,10 @@
 library(shiny)
 library(dplyr)
+library(readxl)
 
 #ça marche bien avec ce document là que je vais mettre sur git, je l'ai légerement modifié genre des accents et des majuscules mais sinon ça marche pas
 
-payss <- read.csv("pays__.csv", sep = ";", encoding = "UTF-8")
+payss <- read_excel("pays__.xlsx")
 head(payss)
 
 ui <- fluidPage(
@@ -70,10 +71,12 @@ ui <- fluidPage(
     sidebarPanel(
       class = "sidebar",
       textInput(inputId = "nom", label = "Votre prénom et nom", placeholder = "Prénom Nom"),
-      numericInput("age", "Votre age:", value = 0, min = 0, max = 99),
+      numericInput("age", "Votre age:", value = 0, max = 99),
       titlePanel("Avec qui souhaitez vous partir ?"),
-      numericInput("adulte", "Nombre d'adulte(s) (+ 16 ans):", value = 0, min = 0, max = 99),
-      numericInput("enfant", "Nombre d'enfant(s) (0-16 ans):", value = 0, min = 0, max = 99),
+      numericInput("adulte", "Nombre d'adulte(s) (+ 16 ans):", value = 0),
+      numericInput("enfant", "Nombre d'enfant(s) (0-16 ans):", value = 0),
+      
+      
       selectInput("saison", "A quelle saison souhaitez vous partir ?:",
                   choices = c("été", "automne","hiver", "printemps")),
       sliderInput("duree",
@@ -97,34 +100,17 @@ ui <- fluidPage(
               tabsetPanel(
                 tabPanel("", tags$p(class = "intro", "Vous êtes en manque d'inspiration pour vos prochaine vacances ? 
              OPEN to the world est là pour vous aider à passer les meilleures vacances de votre vie !"),
-                                                 textOutput("message"))),
-<<<<<<< HEAD
-              tabsetPanel(
-                tabPanel("Récapitulatif", textOutput("message2")),
-                tabPanel("Résultats", textOutput("message3"))
-              ))
-=======
+                         textOutput("message"))),
               tabPanel(class="intro", title = "", textOutput("message2")),
-              tabPanel("resultat",textOutput("message3")),
-              tabPanel("resultat",textOutput("message4"))
-  )
->>>>>>> f608b14ac3212202017bbe8465bdda99ddbbbabe
+              tabPanel("resultat",textOutput("message3")))
   )
 )
 
 
 server <- function(input, output) {
-
+  
   validate_click <- eventReactive(input$validate, {
-    list(nom = input$nom,
-         age = input$age,
-         adulte = input$adulte,
-         enfant = input$enfant,
-         saison = input$saison,
-         duree = input$duree,
-         budget = input$budget,
-         typays = input$typays,
-         typvac = input$typvac)
+    list(age = input$age, nom = input$nom)
   })
   output$message <- renderText({
     req(validate_click())
@@ -137,7 +123,7 @@ server <- function(input, output) {
   output$message2 <- renderText({
     req(validate_click())
     paste("Récapitulons ! Vous êtes", input$nom, "et vous avez", input$age, "ans.
-          Vos vacances se dérouleront en ", input$saison, "pour une durée de ", input$dureee,
+          Vos vacances se dérouleront en ", input$saison, "pour une durée de ", input$duree,
           "jour(s). Vous partirez dans un ", input$typays, "et emmènerez avec vous ", input$adulte , "adulte(s) et ", input$enfant,
           "enfant(s) profiteront d'agréables moments dans un cadre", 
           input$typvac, " avec un budget de ", input$budget, "€/personne." )
@@ -147,48 +133,28 @@ server <- function(input, output) {
     
     #Les boucles marchent !!!!! (je suis trop contente j'ai enfin compris)
     
-    if (input$saison == "été"){PP<- filter(payss, Saison == "été")} 
-    else if (input$saison == "printemps") {PP<- filter(payss, Saison == "printemps")} 
-    else if (input$saison == "hiver") {PP<- filter(payss, Saison == "hiver")} 
-    else if (input$saison == "automne") {PP<- filter(payss, Saison == "automne")}
+    if (input$saison == "été"){PP<- filter(payss, Saison == "été")} else if (input$saison == "printemps") {PP<- filter(payss, Saison == "printemps")} else if (input$saison == "hiver") {PP<- filter(payss, Saison == "hiver")} else if (input$saison == "automne") {PP<- filter(payss, Saison == "automne")}
     
-    if (input$enfant >= 1) {PP <- filter(PP, Enfant == "Oui")} 
-    else {PP<- filter(PP, Enfant == "Non")}
+    if (input$enfant >= 1) {PP <- filter(PP, Enfant == "Oui")} else {PP<- filter(PP, Enfant == "Non")}
     
-    if (input$duree > 15) {PP<- filter(PP, Duree == "Long")} 
-    else if(input$duree < 7) {PP<- filter(PP, Duree == "Court")} 
-    else if(input$duree>7 & duree<14){PP<-filter(PP, Duree == "Moyen")}
+    if (input$duree > 15) {PP<- filter(PP, Duree == "Long")} else if(input$duree < 7) {PP<- filter(PP, Duree == "Court")} else if(input$duree>7 & duree<14){PP<-filter(PP, Duree == "Moyen")}
     
-    if (input$budget > 700) {PP<- filter(PP, Budget == "Fort")} 
-    else if(input$budget < 350) {PP<- filter(PP, Budget == "Faible")} 
-    else if (input$budget>350 & input$budget<700) {PP<-filter(PP, Budget == "Moyen")}
+    if (input$budget > 700) {PP<- filter(PP, Budget == "Fort")} else if(input$budget < 350) {PP<- filter(PP, Budget == "Faible")} else if (input$budget>350 & input$budget<700) {PP<-filter(PP, Budget == "Moyen")}
     
-    if (input$typays == "pays chaud"){PP<- filter(PP, Climat == "Chaud")} 
-    else if(input$typays == "pays froid") {PP<- filter(PP, Climat == "Froid")} 
-    else if (input$typays == "pays tempéré") {PP<- filter(PP, Climat == "Tempéré")}
+    if (input$typays == "pays chaud"){PP<- filter(PP, Climat == "Chaud")} else if(input$typays == "pays froid") {PP<- filter(PP, Climat == "Froid")} else if (input$typays == "pays tempéré") {PP<- filter(PP, Climat == "Tempéré")}
     
-    if (input$typvac == "festif") {PP<- filter(PP, Festives == "Oui")} 
-    else if (input$typvac == "sportif"){PP<- filter(PP, Sportives == "Oui")} 
-    else if (input$typvac == "culturel") {PP<- filter(PP, Culturelles == "Oui")} 
-    else if (input$typvac == "détendu") {PP<- filter(PP, Détentes == "Oui")}
+    if (input$typvac == "festif") {PP<- filter(PP, Festives == "Oui")} else if (input$typvac == "sportif"){PP<- filter(PP, Sportives == "Oui")} else if (input$typvac == "culturel") {PP<- filter(PP, Culturelles == "Oui")} else if (input$typvac == "détendu") {PP<- filter(PP, Détentes == "Oui")}
     
-    renderTable({      req(validate_click())
+    
     PPP<- select(PP, Pays)
-    PPP})
     
     #la ça marche plus j'arrive pas à faire un tirage au sort parmis les données de PPP et je sais comment l'afficher sur Shiny
     
-<<<<<<< HEAD
-    if (is.null(PPP)== TRUE) {paste("Nous n'avons trouvé aucune destination qui corresponde à votre demande")} else{destid<-PPP} 
-=======
-    #if (is.null(PPP)== TRUE) 
-    #return({paste("Nous n'avons trouvé aucune destination qui corresponde à votre demande")} )
-    #else{destid<-PPP} 
->>>>>>> f608b14ac3212202017bbe8465bdda99ddbbbabe
+    if (is.null(PPP)== TRUE) {paste("Nous n'avons trouvé aucune destination qui corresponde à votre demande")} else{destid<-PPP)} 
     
     #aussi il faudrait essayer de mettre le message3 sur l'autre onglet
     
-})
+  })
 }
 
 # Exécution de l'application
